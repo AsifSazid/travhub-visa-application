2ff41d5323aae1658206251f3236bcac8c195629
AsifSazid <asif83sazi@gmail.com>
2025-12-03 17:03:55 +0600

    Get Ready

--- server/db_connection.php
+++ server/db_connection.php
@@ -1,6 +1,5 @@
 <?php
 
-$pdo = new PDO("mysql:host=localhost;dbname=visa_application", "root", "");
-// $pdo = new PDO("mysql:host=localhost;dbname=sazummec_travhub_visa_apply", "
-// sazummec_common_root", "C0ww0nR001");
+// $pdo = new PDO("mysql:host=localhost;dbname=visa_application", "root", "");
+$pdo = new PDO("mysql:host=localhost;dbname=sazummec_travhub_visa_apply", "sazummec_common_root", "C0ww0nR001");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
